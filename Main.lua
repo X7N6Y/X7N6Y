8de91be140d1960f16c81c17cbc5db83dbb79ed7
Azure <yaniifffff@gmail.com>
2024-12-27 22:19:50 +0700

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -6,135 +6,168 @@ local gameId = 16732694052
 
 if game.PlaceId == gameId then
 
-    local Window = Fluent:CreateWindow({
-        Title = "Fluent " .. Fluent.Version,
-        SubTitle = "by dawid",
-        TabWidth = 160,
-        Size = UDim2.fromOffset(580, 460),
-        Acrylic = true,
-        Theme = "Dark",
-        MinimizeKey = Enum.KeyCode.LeftControl
-    })
+	local Window = Fluent:CreateWindow({
+		Title = "Fluent " .. Fluent.Version,
+		SubTitle = "by dawid",
+		TabWidth = 160,
+		Size = UDim2.fromOffset(580, 460),
+		Acrylic = true,
+		Theme = "Dark",
+		MinimizeKey = Enum.KeyCode.LeftControl
+	})
 
-    local Tabs = {
-        Home = Window:AddTab({Title = "Home", Icon = "home"}),
-        Main = Window:AddTab({ Title = "Main", Icon = "menu" }),
-        Settings = Window:AddTab({ Title = "Settings", Icon = "settings" }),
-    }
+	local Tabs = {
+		Home = Window:AddTab({Title = "Home", Icon = "home"}),
+		Main = Window:AddTab({ Title = "Main", Icon = "menu" }),
+		Settings = Window:AddTab({ Title = "Settings", Icon = "settings" }),
+	}
 
-    local Options = Fluent.Options
+	local Options = Fluent.Options
 
-    local autoCastEnabled = false
-    local autoReelEnabled = false
-    local lastCastTime = 0
-    local reelFinished = true
-    local casting = false
-    local rodDetected = false
+    local Player = game:GetService("Players")
+    local LocalPlayer = Player.LocalPlayer
+    local Char = LocalPlayer.Character
+    local Humanoid = Char.Humanoid
+    local VirtualInputManager = game:GetService("VirtualInputManager")
+    local ReplicatedStorage = game:GetService("ReplicatedStorage")
+    local GuiService = game:GetService("GuiService")
     local currentRod = nil
-    local lastManualCast = 0
+    local rodEquipped = false
 
-    local function detectRod()
-        currentRod = game:GetService("Players").LocalPlayer.Backpack:FindFirstChildOfClass("Tool") or game:GetService("Players").LocalPlayer.Character:FindFirstChildOfClass("Tool")
-        if currentRod and currentRod:FindFirstChild("events") and currentRod.events:FindFirstChild("cast") then
-            rodDetected = true
-        else
-            rodDetected = false
-        end
-    end
 
-    local function castRod()
-        if rodDetected then
-            currentRod.events.cast:FireServer(100, 1)
-            lastCastTime = tick()
-            reelFinished = false
-              if autoReelEnabled then
-                  local connection
-                  connection = game:GetService("ReplicatedStorage").events.reelfinished.OnClientEvent:Connect(function()
-                      connection:Disconnect()
-                      reelFinished = true
-                  end)
-            end
-        end
-    end
-
-    task.spawn(function()
-        while true do
-            detectRod()
-            task.wait(1)
-        end
-    end)
-
-    local AutoCastToggle = Tabs.Main:AddToggle("AutoCast", {Title = "Auto Cast", Default = false})
-    AutoCastToggle:OnChanged(function()
-        autoCastEnabled = Options.AutoCast.Value
-        if autoCastEnabled then
-            task.spawn(function()
-                while autoCastEnabled do
-                    if rodDetected and not casting and (tick() - lastCastTime >= 0.2) then
-                        casting = true
-                        castRod()
-                        lastCastTime = tick()
-                    else
-                        casting = false
-                    end
-                    task.wait()
+	do
+		
+        _G.AutoCast = false
+        task.spawn(function()
+            while true do
+                task.wait()
+                if _G.AutoCast and rodEquipped then
+                    pcall(function()
+                        if currentRod then
+                            task.wait(.1)
+                            currentRod.events.cast:FireServer(100,1)
+                        end
+                    end)
                 end
-            end)
-        end
-    end)
+            end
+        end)
 
-     local castButton = Tabs.Main:AddButton({
-        Title = "Perfect Cast",
-        Description = "Cast your rod",
-    })
 
-     castButton.OnClicked(function()
-         if (tick() - lastManualCast >= 0.5) then
-            lastManualCast = tick()
-            castButton.Text = "Casting..."
-            castRod()
-             task.wait()
-             castButton.Text = "Perfect Cast"
-         end
-     end)
+        _G.AutoShake = false
+        task.spawn(function()
+            while true do
+                task.wait()
+                if _G.AutoShake then
+                    pcall(function()
+                         task.wait(0.01)
+                        local PlayerGUI = game:GetService("Players").LocalPlayer:WaitForChild("PlayerGui")
+                        local shakeUI = PlayerGUI:FindFirstChild("shakeui")
+                        if shakeUI and shakeUI.Enabled then
+                            local safezone = shakeUI:FindFirstChild("safezone")
+                            if safezone then
+                                local button = safezone:FindFirstChild("button")
+                                if button and button:IsA("ImageButton") and button.Visible then
+                                        GuiService.SelectedObject = button
+                                        VirtualInputManager:SendKeyEvent(true, Enum.KeyCode.Return, false, game)
+                                        VirtualInputManager:SendKeyEvent(false, Enum.KeyCode.Return, false, game)
+                                end
+                            end
+                        end
+                    end)
+                end
+            end
+        end)
+        -- AutoReel
+        _G.AutoReel = false
+        task.spawn(function()
+            while true do
+                task.wait()
+                if _G.AutoReel then
+                    pcall(function()
+                        for i,v in pairs(LocalPlayer.PlayerGui:GetChildren()) do
+                            if v:IsA "ScreenGui" and v.Name == "reel"then
+                                if v:FindFirstChild "bar" then
+                                    task.wait(.15)
+                                        ReplicatedStorage.events.reelfinished:FireServer(100,true)
+                                end
+                            end
+                        end
+                    end)
+                end
+            end
+        end)
 
-    local AutoReelToggle = Tabs.Main:AddToggle("AutoReel", {Title = "Auto Reel", Default = false})
-    AutoReelToggle:OnChanged(function()
-        autoReelEnabled = Options.AutoReel.Value
-    end)
+        task.spawn(function()
+            while true do
+                task.wait(1)
+                currentRod = Char:FindFirstChildOfClass("Tool")
+                if currentRod and currentRod.Name:lower():find("rod") then
+                    rodEquipped = true
+                else
+                    rodEquipped = false
+                end
+            end
+        end)
 
-    Tabs.Home:AddParagraph({
-        Title = "Credit",
-        Content = "Made by Azure"
-    })
-    Tabs.Home:AddButton({
-        Title = "Discord (Copy)",
-        Description = "Copy the discord link to your clipboard",
-        Callback = function()
-            setclipboard("https://discord.gg/cUujpKKdtX")
-        end
-    })
+		local AutoCastToggle = Tabs.Main:AddToggle("AutoCast", {Title = "Auto Cast", Default = false})
+		AutoCastToggle:OnChanged(function()
+			_G.AutoCast = Options.AutoCast.Value
+		end)
+		
+		Tabs.Main:AddButton({
+			Title = "Perfect Cast",
+			Description = "Cast your rod",
+			Callback = function()
+                if currentRod then
+					currentRod.events.cast:FireServer(100,1)
+				end
+			end
+		})
 
-    SaveManager:SetLibrary(Fluent)
-    InterfaceManager:SetLibrary(Fluent)
+		local AutoReelToggle = Tabs.Main:AddToggle("AutoReel", {Title = "Auto Reel", Default = false})
+		AutoReelToggle:OnChanged(function()
+			_G.AutoReel = Options.AutoReel.Value
+		end)
+        
+        local AutoShakeToggle = Tabs.Main:AddToggle("AutoShake", {Title = "Auto Shake", Default = false})
+        AutoShakeToggle:OnChanged(function()
+            _G.AutoShake = Options.AutoShake.Value
+        end)
+		
+		Tabs.Home:AddParagraph({
+			Title = "Credit",
+			Content = "Made by Azure"
+		})
+		Tabs.Home:AddButton({
+			Title = "Discord (Copy)",
+			Description = "Copy the discord link to your clipboard",
+			Callback = function()
+				setclipboard("https://discord.gg/cUujpKKdtX")
+				
+			end
+		})
+	end
 
-    SaveManager:IgnoreThemeSettings()
-    SaveManager:SetIgnoreIndexes({})
-    InterfaceManager:SetFolder("FluentScriptHub")
-    SaveManager:SetFolder("FluentScriptHub/specific-game")
+	SaveManager:SetLibrary(Fluent)
+	InterfaceManager:SetLibrary(Fluent)
 
-    InterfaceManager:BuildInterfaceSection(Tabs.Settings)
-    SaveManager:BuildConfigSection(Tabs.Settings)
+	SaveManager:IgnoreThemeSettings()
+	SaveManager:SetIgnoreIndexes({})
+	InterfaceManager:SetFolder("FluentScriptHub")
+	SaveManager:SetFolder("FluentScriptHub/specific-game")
 
-    Window:SelectTab(1)
+	InterfaceManager:BuildInterfaceSection(Tabs.Settings)
+	SaveManager:BuildConfigSection(Tabs.Settings)
 
-    Fluent:Notify({
-        Title = "Fluent",
-        Content = "The script has been loaded.",
-        Duration = 8
-    })
+	Window:SelectTab(1)
 
-    SaveManager:LoadAutoloadConfig()
+	Fluent:Notify({
+		Title = "Fluent",
+		Content = "The script has been loaded.",
+		Duration = 8
+	})
+
+	SaveManager:LoadAutoloadConfig()
 else
-    game.Players.LocalPlayer:Kick("Game not supported")
+	game.Players.LocalPlayer:Kick("Game not supported")
 end
