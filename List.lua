f5ba144900b93dd3bf4d73ede2ab76ce7e3299f1
Azure <yaniifffff@gmail.com>
2025-02-01 18:45:09 +0700

    Update List.lua
    Minified

--- List.lua
+++ List.lua
@@ -1,15 +1,1 @@
-local Games = {
-    [1537690962] = "https://raw.githubusercontent.com/YourName/YourRepo/main/script1.lua",
-    [2753915549] = "https://raw.githubusercontent.com/YourName/YourRepo/main/script2.lua",
-    [4442272183] = "https://raw.githubusercontent.com/YourName/YourRepo/main/script3.lua",
-    [7449423635] = "https://raw.githubusercontent.com/YourName/YourRepo/main/script4.lua",
-    [3623096087] = "https://raw.githubusercontent.com/YourName/YourRepo/main/script5.lua",
-    [8737899170] = "https://raw.githubusercontent.com/YourName/YourRepo/main/script6.lua",
-    [16498369169] = "https://raw.githubusercontent.com/YourName/YourRepo/main/script7.lua",
-    [17503543197] = "https://raw.githubusercontent.com/YourName/YourRepo/main/script8.lua",
-    [10449761463] = "https://raw.githubusercontent.com/YourName/YourRepo/main/script9.lua",
-    [18901165922] = "https://raw.githubusercontent.com/YourName/YourRepo/main/script10.lua",
-    [16732694052] = "https://raw.githubusercontent.com/X7N6Y/X7N6Y/refs/heads/main/Fisch.lua",
-}
-
-return Games
+return{[1537690962]="https://raw.githubusercontent.com/YourName/YourRepo/main/script1.lua",[2753915549]="https://raw.githubusercontent.com/YourName/YourRepo/main/script2.lua",[4442272183]="https://raw.githubusercontent.com/YourName/YourRepo/main/script3.lua",[7449423635]="https://raw.githubusercontent.com/YourName/YourRepo/main/script4.lua",[3623096087]="https://raw.githubusercontent.com/YourName/YourRepo/main/script5.lua",[8737899170]="https://raw.githubusercontent.com/YourName/YourRepo/main/script6.lua",[16498369169]="https://raw.githubusercontent.com/YourName/YourRepo/main/script7.lua",[17503543197]="https://raw.githubusercontent.com/YourName/YourRepo/main/script8.lua",[10449761463]="https://raw.githubusercontent.com/YourName/YourRepo/main/script9.lua",[18901165922]="https://raw.githubusercontent.com/YourName/YourRepo/main/script10.lua",[16732694052]="https://raw.githubusercontent.com/X7N6Y/X7N6Y/refs/heads/main/Fisch.lua"}
