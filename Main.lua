7dda1768d7cc8d2e415c2267a8f2a3fd1938b162
Azure <yaniifffff@gmail.com>
2025-02-02 12:36:16 +0700

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -1,4 +1,5 @@
-loadstring(game:HttpGet('https://raw.githubusercontent.com/X7N6Y/X7N6Y/refs/heads/main/List.lua'))()
+local Games = loadstring(game:HttpGet('https://raw.githubusercontent.com/X7N6Y/X7N6Y/refs/heads/main/List.lua'))()
+
 for PlaceID, Execute in pairs(Games) do
     if PlaceID == game.PlaceId then
         loadstring(game:HttpGet(Execute))()
